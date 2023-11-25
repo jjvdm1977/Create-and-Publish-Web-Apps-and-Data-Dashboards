fb1d3dd5779f28d5c735f4a96c11193a575410b2
Johannesvan5 <8222549-Johannesvan5@users.noreply.replit.com>
2023-11-25 11:59:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First Stremlit App')
+st.subheader('Introducing Streamlit and Automate Everything with Python')
+st.write('''This is our first web App
+Enjoy it''')
+
